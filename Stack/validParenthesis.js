10437db5a278b9a8ceb914e64392192919019e7e
Neena <neenarajendran1999@gmail.com>
2025-01-13 22:39:49 +0530

    New Qns

--- Stack/validParenthesis.js
+++ Stack/validParenthesis.js
@@ -1,10 +1,26 @@
+function isValid(s){
+    let pairs = {'(':')','{':'}','[':']'}
+    let n= s.length
+    if(n%2===1) return false
+    if(s[0]==='}'||s[0]===']'||s[0]===')') return false
+    if(s[n-1]==='('||s[n-1]==='{'||s[n-1]==='[') return false
+
+    let stack =[]
+    for(let i=0;i<s.length;i++){
+        if(s[i]==='('||s[i]==='{'||s[i]==='['){
+            stack.push(s[i])
+        }else if(pairs[stack.pop()]!== s[i]){
+            return false
+        }
+    }
+    return stack.length === 0
+}
 let str1 ='{}{}[][))'
 let str2 ='({{[]}})'
 let str3 = '(){}[]'
 let str4 = '[{{(])})}]'
 
-function isValid(s){
-    for(let s of str){
-        
-    }
-}
+console.log(isValid(str1));
+console.log(isValid(str2));
+console.log(isValid(str3));
+console.log(isValid(str4));
